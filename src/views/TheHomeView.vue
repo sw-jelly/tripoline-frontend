f38c7c317c3797959ede3e5f55f585a15bf3ad7f
DESKTOP-64649GT\Master <crm03008@gmail.com>
2023-11-13 21:04:17 +0900

    carousel 기능 적용 (#1)

--- src/views/TheHomeView.vue
+++ src/views/TheHomeView.vue
@@ -1,10 +1,42 @@
 <script setup>
-// import TheWelcome from '../components/TheWelcome.vue'
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+import { ref, reactive } from 'vue'
+
+const videoURL = ref([
+  {
+    src: new URL('@/assets/video/cherry.mp4', import.meta.url).href
+  },
+  {
+    src: new URL('@/assets/video/beach.mp4', import.meta.url).href
+  },
+  {
+    src: new URL('@/assets/video/seoul.mp4', import.meta.url).href
+  }
+])
 </script>
 
 <template>
   <div class="vh-100 text-white rounded">
-    <video
+    <carousel :items-to-show="1" :autoplay="10000" :wrap-around="true">
+      <slide v-for="video in videoURL" :key="video.src" style="width: 100vw; height: 100vh">
+        <video
+          style="width: 100vw; height: 100%; object-fit: cover"
+          autoplay
+          muted
+          loop
+          data-aos="fade-in"
+          :src="video.src"
+          type="video/mp4"
+        ></video>
+      </slide>
+
+      <template #addons>
+        <navigation />
+        <pagination />
+      </template>
+    </carousel>
+    <!-- <video
       style="width: 100%; height: 100%; object-fit: cover"
       autoplay
       muted
@@ -12,7 +44,7 @@
       data-aos="fade-in"
       src="@/assets/video/cherry.mp4"
       type="video/mp4"
-    ></video>
+    ></video> -->
     <div class="container">
       <div class="row">
         <div class="col-lg-10">
